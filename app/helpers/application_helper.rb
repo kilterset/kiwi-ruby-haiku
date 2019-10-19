386c8cb67615dcdf95c5b1de03e1e7a8da9e20c0
Pete Nicholls <pn@trineo.com>
2019-10-19 17:18:36 +1300

    Style app to match design
    No CSS framework has been used, as the app is very simple and the
    cost of overhead of maintaining the dependency and the increased page
    load size does not seem worth the negligible benefit.
    
    No web fonts have been added, to keep the overhead of page load to a
    minimum. Close substitutes have been chosen, although they need testing
    and tweaking on Android and Windows.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def app_name
+    translate :app_name
+  end
+
+  def project_repository_url
+    translate :project_repository_url
+  end
 end
